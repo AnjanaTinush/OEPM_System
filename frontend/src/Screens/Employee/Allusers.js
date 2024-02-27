8b87754a436df14d92fa4b363b0a87f774d7b2fe
AnjanaTinush <anjanatinush222@gmail.com>
2024-02-27 23:22:53 +0530

    create add user for admin

--- frontend/src/Screens/Employee/Allusers.js
+++ frontend/src/Screens/Employee/Allusers.js
@@ -1,13 +1,94 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState,useRef } from 'react'
 import axios, { Axios } from "axios";
 import { Link } from "react-router-dom";
 import { MdDeleteForever } from "react-icons/md";
-import { GrUpdate } from "react-icons/gr";
+import { FaEdit } from "react-icons/fa";
 import Adminnavbar from './Component/Adminnavbar'
 import Navbar from './Component/Navbar'
+import Loader from '../../Component/Loader';
 
 function Allusers() {
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const modalRef = useRef(null);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleOutsideClick = (e) => {
+    if (modalRef.current && !modalRef.current.contains(e.target)) {
+      closeModal();
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formDataObject = {};
+    formData.forEach((value, key) => {
+      formDataObject[key] = value;
+    });
+    console.log(formDataObject);
+    closeModal(); // Close the modal after form submission
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
+
+
+  const[name , setname]=useState('')
+  const[email , setemail]=useState('')
+  const[phone , setphone]=useState('')
+  const[password , setpassword]=useState('')
+  const [cpassword, setcpassword] = useState('');
+
+const [Loading,setLoading] = useState(false)
+
+//add users 
+ async function adduser(event){
+  event.preventDefault();
+
+  if(password==cpassword){
+    const user = {
+      name,
+      email,
+      phone,
+      password,
+      cpassword,
+    };
+
+    try {
+      setLoading(true);
+      const result = await axios.post("http://localhost:5000/api/users/register",user);
+      setLoading(false);
+      console.log(result.data);
+      closeModal();
+      window.location.reload()
+
+    } catch (error) {
+
+      console.log(error)
+      setLoading(false)  
+    }
+
+  }else{
+    alert("Password dosen't match...")
+  }
+
+ }
+
+
+
+//read data
 const [users,setusers] = useState([]);
 
 const  fetchData = async() =>{
@@ -15,6 +96,7 @@ const  fetchData = async() =>{
       try {
         const data = await axios.get("http://localhost:5000/api/users/getallusers");
         setusers(data.data)
+        
       } catch (error) {
         console.log(error)
       }  
@@ -26,6 +108,8 @@ useEffect(()=>{
 
 
 
+
+//delete users
 async function deleteuser(id){
 
   try {
@@ -38,6 +122,10 @@ async function deleteuser(id){
 
 }
 
+
+
+
+
   return (
     <div className='bg-wight-green'>
         <div className="flex">
@@ -49,9 +137,100 @@ async function deleteuser(id){
         <Navbar />
         <br></br>
         <br></br>
-        <br></br>
+      
        
-        
+       
+
+
+        <div className="flex justify-end items-center mr-20 h-full ">
+            <button
+              type="button"
+              class="text-white bg-whatsapp-green hover:bg-Buttongreen focus:outline-none focus:ring-4 focus:ring-Buttongreen font-medium rounded-full text-me px-5 py-2.5 text-center me-2 mb-2 dark:whatsapp-green dark:hover:bg-Buttongreen dark:focus:ring-Buttongreen font-sans shadow-xl"
+              onClick={openModal}
+            >
+              Click Here to Add a Users
+            </button>
+          </div>
+
+          {/* Modal */}
+          {isModalOpen && (
+            <div className="fixed inset-0 z-10 overflow-y-auto bg-gray-500 bg-opacity-50 flex justify-center items-center backdrop-blur-sm">
+             
+              <div
+                ref={modalRef}
+                className="bg-white rounded-3xl overflow-hidden shadow-xl max-w-md"
+              >
+                <div className="px-12 py-12">
+               
+
+                  
+                  <h2 className="text-xl font-semibold text-dark font-custom  ">
+                    Enter the users details
+                  </h2>
+                  {Loading && (<Loader/>)}
+                  <form onSubmit={handleSubmit} className="mt-8">
+                    <div>
+                      <input
+                        type="text"
+                        placeholder="Enter user name"
+                        value={name}
+                        onChange={(e)=>{setname(e.target.value)}}
+                        className="mt-1 p-2 block w-full rounded-3xl bg-wight-green border-none focus:outline-whatsapp-green placeholder-gray-500 placeholder-opacity-50 font-custom text-md "
+                       
+                      />
+                    </div>
+                    <div className="mt-4">
+                      <input
+                        type="email"
+                        placeholder="Enter email"
+                        value={email}
+                        onChange={(e)=>{setemail(e.target.value)}}
+                        className="mt-1 p-2 block w-full rounded-3xl bg-wight-green border-none focus:outline-whatsapp-green placeholder-gray-500 placeholder-opacity-50 font-custom text-md "
+                        
+                      />
+                    </div>
+                    <div className="mt-4">
+                      <input
+                        type="password"
+                        placeholder="Enter passwoard"
+                        value={password}
+                        onChange={(e)=>setpassword(e.target.value)}
+                        className="mt-1 p-2 block w-full rounded-3xl bg-wight-green border-none focus:outline-whatsapp-green placeholder-gray-500 placeholder-opacity-50 font-custom text-md "
+                       
+                      />
+                    </div>
+                    <div className="mt-4">
+                      <input
+                        type="password"
+                        placeholder="confirm password"
+                        value={cpassword}
+                        onChange={(e)=>setcpassword(e.target.value)}
+                        className="mt-1 p-2 block w-full rounded-3xl bg-wight-green border-none focus:outline-whatsapp-green placeholder-gray-500 placeholder-opacity-50 font-custom text-md "
+                       
+                      />
+                    </div>
+
+                    <div className="mt-8 mb-2">
+                      <button
+                        type="submit"
+                        className="text-white bg-dark hover:bg-darkhover block w-full focus:outline-none  font-semibold rounded-md font-custom text-md px-5 py-2.5 text-center me-2 mb-2 dark:bg-dark dark:hover:bg-darkhover"
+                      
+                        onClick={(e)=>adduser(e)}
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          )}
+       
+       
+       
+       
+       
+       
         <div className="flex justify-center items-center h-full">
         
         <div className="overflow-x-auto shadow-2xl sm:rounded-lg ml-60">
@@ -102,11 +281,11 @@ async function deleteuser(id){
             </td>
             <td className="px-6 py-4 text-right text-green-900">
             <Link to={`#`}>
-                      <button className="btn1 mr-3"><GrUpdate    className="mr-5 text-lg" />
+                      <button className="btn1 mr-3"><FaEdit    className="mr-5 text-xl" />
             </button>
             </Link> 
             <Link to={`#`}>
-                      <button className="btn1" onClick={(e)=>deleteuser(users._id)}><MdDeleteForever   className="mr-5 text-lg" />
+                      <button className="btn1" onClick={(e)=>deleteuser(users._id)}><MdDeleteForever   className="mr-5 text-2xl " />
             </button>
             </Link>  
           </td>
@@ -114,7 +293,10 @@ async function deleteuser(id){
          ))}
         </tbody>
       </table>
+
       
+     
+
     </div>
     
     </div>
@@ -122,20 +304,17 @@ async function deleteuser(id){
         <br></br>
         <br></br>
         <br></br>
-        <br></br><br></br>
         <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-       
-       
+      
+      
     </div>
     </div>
     </div>
   )
 }
 
-export default Allusers
+export default Allusers;
+
+
+
+    
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,10 +5,14 @@ module.exports = {
     extend: {
       colors: {
         "whatsapp-green": "#48c81b",
-        "wight-green": "#f5fcf4",
+        "wight-green": "#f5f5f5",
         "black-green": "#132A13",
         "table-row": "#C5D7C9",
-        "tablerow-hover": "#DBF4E0"
+        "tablerow-hover": "#DBF4E0",
+        "Buttongreen": "#29bf12",
+        "dark": "#133d00",
+        "darkhover": "#3b562f",
+
       },
      
   fontFamily: {
