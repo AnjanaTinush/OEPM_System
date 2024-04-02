0e1ce0771c5b7ac5ee4dcaa37584002694ff451d
AnjanaTinush <anjanatinush222@gmail.com>
2024-04-02 16:28:31 +0530

    modify admin navigation bar

--- frontend/src/Screens/Employee/Allusers.js
+++ frontend/src/Screens/Employee/Allusers.js
@@ -1,197 +1,187 @@
 import React, { useEffect, useState,useRef } from 'react'
-import axios, { Axios } from "axios";
-import { Link } from "react-router-dom";
-import { MdDeleteForever } from "react-icons/md";
-import { FaEdit } from "react-icons/fa";
-import Adminnavbar from './Component/Adminnavbar'
-import Navbar from '../Component/Navbar';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { MdDeleteForever } from 'react-icons/md';
+import { FaEdit } from 'react-icons/fa';
+import Swal from 'sweetalert2';
+import Adminnavbar from './Component/Adminnavbar';
 import Loader from '../../Component/Loader';
-import Userupdate from './Userupdate';
+import { CiSearch } from "react-icons/ci";
+import toast from 'react-hot-toast';
 import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
 
 AOS.init({
-  duration:2500
+  duration: 2500,
 });
-
+//read users
 function Allusers() {
+  const [users, setUsers] = useState([]);
+  const [searchKey, setSearchKey] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const [duplicateusers, setduplicateusers] = useState([]);
-  const [searchkey , setsearchkey] = useState();
-  const [loading, setloading] = useState(false);
-
-  //Add users popup form
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const modalRef = useRef(null);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleOutsideClick = (e) => {
-    if (modalRef.current && !modalRef.current.contains(e.target)) {
-      closeModal();
+  const fetchData = async () => {
+    try {
+      const data = await axios.get('http://localhost:5000/api/users/getallusers');
+      setUsers(data.data);
+    } catch (error) {
+      console.log(error);
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const formDataObject = {};
-    formData.forEach((value, key) => {
-      formDataObject[key] = value;
-    });
-    console.log(formDataObject);
-    closeModal(); // Close the modal after form submission
-  };
-
   useEffect(() => {
-    document.addEventListener("mousedown", handleOutsideClick);
-    return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
-    };
+    fetchData();
   }, []);
 
-//add users 
+   //Add users popup form
+   const [isModalOpen, setIsModalOpen] = useState(false);
+   const modalRef = useRef(null);
+ 
+   const openModal = () => {
+     setIsModalOpen(true);
+   };
+ 
+   const closeModal = () => {
+     setIsModalOpen(false);
+   };
+ 
+   const handleOutsideClick = (e) => {
+     if (modalRef.current && !modalRef.current.contains(e.target)) {
+       closeModal();
+     }
+   };
+ 
+   const handleSubmit = (e) => {
+     e.preventDefault();
+     const formData = new FormData(e.target);
+     const formDataObject = {};
+     formData.forEach((value, key) => {
+       formDataObject[key] = value;
+     });
+     console.log(formDataObject);
+     closeModal(); // Close the modal after form submission
+   };
+ 
+   useEffect(() => {
+     document.addEventListener("mousedown", handleOutsideClick);
+     return () => {
+       document.removeEventListener("mousedown", handleOutsideClick);
+     };
+   }, []);
+ 
+
+  //add users for the System
   const[name , setname]=useState('')
   const[email , setemail]=useState('')
   const[phone , setphone]=useState('')
   const[password , setpassword]=useState('')
   const [cpassword, setcpassword] = useState('');
 
-const [Loading,setLoading] = useState(false)
 
 
- async function adduser(event){
-  event.preventDefault();
+  async function adduser(event) {
+    event.preventDefault();
+  
+    if (password == cpassword) {
+      const user = {
+        name,
+        email,
+        phone,
+        password,
+        cpassword,
+      };
+  
+      try {
+        const result = await axios.post("http://localhost:5000/api/users/register", user);
+  
+        console.log(result.data);
+        closeModal();
+        Swal.fire(
+          "Congratulations",
+          "User added successfully",
+          "success"
+        ).then(() => {
+          window.location.reload();
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      toast.error("Password dosen't match");
+    }
+  }
+  
 
-  if(password==cpassword){
-    const user = {
-      name,
-      email,
-      phone,
-      password,
-      cpassword,
-    };
 
+
+
+  //delete user
+  const deleteuser = async (id) => {
     try {
       setLoading(true);
-      const result = await axios.post("http://localhost:5000/api/users/register",user);
-      setLoading(false);
-      console.log(result.data);
-      closeModal();
-      window.location.reload()
+      const confirmed = await Swal.fire({
+        title: 'Are you sure remove user?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!',
+      });
 
+      if (confirmed.isConfirmed) {
+        await axios.delete(`http://localhost:5000/api/users/delete/${id}`);
+        fetchData(); // Reload data after successful deletion
+        Swal.fire({
+          icon: 'success',
+          title: 'Success',
+          text: 'User deleted successfully!',
+        });
+      }
     } catch (error) {
-
-      console.log(error)
-      setLoading(false)  
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-  }else{
-    alert("Password dosen't match...")
-  }
+//search user
+const [searchuser, setsearchuser] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
- }
-
-
-
-//read data
-const [users,setusers] = useState([]);
-
-const  fetchData = async() =>{
-
-      try {
-        const data = await axios.get("http://localhost:5000/api/users/getallusers");
-        setusers(data.data)
-        
-      } catch (error) {
-        console.log(error)
-      }  
-};
-
-useEffect(()=>{
-  fetchData();
-},[]);
-
-
-
-
-//delete users
-async function deleteuser(id){
-
-  try {
-    setLoading(true);
-    const response = (await axios.delete(`http://localhost:5000/api/users/delete/${id}`)).data
-    setLoading(false);
-    console.log(response)
-    window.location.reload()
-  } catch (error) {
-    console.log(error)
-    setLoading(false);
-  }
-
-}
-
-
-
- function filterBySearch(key) {
-    // Filter users based on searchKey
-    const tempUsers = users.filter(user => user.name.toLowerCase().includes(key.toLowerCase()));
-    setusers(tempUsers);
-  }
+  const filteredUsers = users.filter((user) =>
+  user.name.toLowerCase().includes(searchQuery.toLowerCase())
+);
 
 
   return (
-    <div className='bg-wight-green'>
-      
-        <div className="flex">
-      {/* Side Navigation */}
+    <div >
       <Adminnavbar />
-
-      <div className="flex flex-col w-full">
-        {/* Top Navigation */}
-        <Navbar />
-        
-      
-
-
-        <div className="flex justify-end items-center mr-20 h-full ">
-
-       
-        <form class="max-w-md mx-auto py-3 mt-5 ">     
-        {Loading && (<Loader/>)}
-    <label for="default-search" class="mb-2 text-sm font-medium text-whatsapp-green sr-only dark:text-dark ">Search</label>
-    <div class="relative mr-60">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
-        </div>
-        <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm border border-whatsapp-green rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search user" required 
-value={searchkey}
-onChange={(e) => { setsearchkey(e.target.value); filterBySearch(e.target.value); }}        />
-        {/* <button type="submit" class="text-white absolute end-2.5 bottom-2.5  hover:bg-Buttongreen focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 dark:bg-dark dark:hover:hover:bg-Buttongreen dark:focus:ring-blue-800">Search</button> */}
-    </div>
-</form>
+   
+     
+   
+      <div className='flex justify-between items-center mx-20  h-full mt-10'>
+    <input
+    
+        type="text"
+        placeholder="Search  name..."
+        className="mt-4 p-2 pl-10 block w-72 ml-64 mb-4 rounded-3xl bg-wight-green border-solid border-2 border-gray focus:outline-whatsapp-green placeholder-gray-500 placeholder-opacity-50 font-custom text-md"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+    />
+    
+    <button
+        type="button"
+        className="text-white bg-whatsapp-green hover:bg-Buttongreen focus:outline-none focus:ring-4 focus:ring-Buttongreen font-medium rounded-full text-me px-6 py-1 text-center me-2 mb-0 dark:whatsapp-green dark:hover:bg-Buttongreen dark:focus:ring-Buttongreen font-sans shadow-xl"
+        onClick={openModal}
+    >
+        Add
+    </button>
+</div>
 
 
-
-            <button
-              type="button"
-              class="text-white bg-whatsapp-green hover:bg-Buttongreen focus:outline-none focus:ring-4 focus:ring-Buttongreen font-medium rounded-full text-me px-6 py-1 mt-7 text-center me-2 mb-0 dark:whatsapp-green dark:hover:bg-Buttongreen dark:focus:ring-Buttongreen font-sans shadow-xl"
-              onClick={openModal}
-            >
-           Add
-            </button>
-          </div>
-
-          {/* Modal */}
-          {isModalOpen && (
+       {/* Modal */}
+       {isModalOpen && (
             <div className="fixed inset-0 z-10 overflow-y-auto bg-gray-500 bg-opacity-50 flex justify-center items-center backdrop-blur-sm">
              
               <div
@@ -263,103 +253,73 @@ onChange={(e) => { setsearchkey(e.target.value); filterBySearch(e.target.value);
               </div>
             </div>
           )}
-       
-       
-       
-       
-       
-       
-        <div className="flex justify-center items-center ml-48 h-full">      
-        <div className="overflow-x-auto shadow-2xl sm:rounded-lg ml-16"> {Loading && (<Loader/>)}
-      <table data-aos="zoom out" className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-whatsapp-green uppercase bg-wight-green dark:bg-whatsapp-green dark:text-wight-green">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              id
-            </th>
-            <th scope="col" className="px-6 py-3">
-              name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              email
-            </th>
-            <th scope="col" className="px-6 py-3">
-              image
-            </th>
-            <th scope="col" className="px-6 py-3">
-              roll
-            </th>
-            <th scope="col" className="px-6 py-3">
-             Action
-            </th>
-          </tr>
-        </thead>
-       
-        <tbody>
-          {users.length > 0 && 
-          users.map((users)=>(
-          <tr className="bg-white dark:bg-table-row  hover:tablerow-hover dark:hover:bg-tablerow-hover">
-            <td className="px-6 py-4 font-medium text-green-900 ">
-            {users._id}
-            </td>
-            <td className="px-6 py-4 text-green-900">
-              {users.name}
-            </td>
-            <td className="px-6 py-4 text-green-900">
-              {users.email}
-            </td>
-            <td className="px-6 py-4 text-green-900">
-              <img className='w-10 rounded-full '
-              src={users.imageurl}
-              alt='profile'
-              />
-            </td>
-            <td className="px-6 py-4 text-green-900">
-              {users.role}
-            </td>
-            <td className="px-6 py-4 text-right text-green-900">
-            <Link to={`/e_updates/${users._id}`}>
-  <button className="btn1 mr-3"  >
-    <FaEdit className="mr-5 text-xl"  />
-  
 
-  </button>
-</Link>
+      <div className='flex justify-center items-center ml-48 h-full '>
+        <div className='overflow-x-auto shadow-2xl sm:rounded-lg ml-16 mb-4'>
+          {loading && <Loader />}
+          <table data-aos='zoom out' className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 '>
+            <thead className='text-xs text-whatsapp-green uppercase bg-wight-green dark:bg-whatsapp-green dark:text-wight-green'>
+           
+              <tr>
+                <th scope='col' className='px-6 py-3'>
+                  id
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  name
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  email
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  image
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  roll
+                </th>
+                <th scope='col' className='px-6 py-3'>
+                  Action
+                </th>
+              </tr>
+           
+            </thead>
 
-
-            <Link to={`#`}> 
-                      <button className="btn1" onClick={(e)=>deleteuser(users._id)}><MdDeleteForever   className="mr-5 text-2xl " />
-            </button>
-            </Link>  
-          </td>
-          </tr>
-         ))}
-        </tbody>
-      </table>
-
-      
-     
-
+            <tbody>
+            {filteredUsers.length === 0 ? (
+  <tr>
+    <td colSpan="6" className="px-6 py-4 text-center text-red-500">
+      User not found...
+    </td>
+  </tr>
+) : (
+            filteredUsers.map((user) => (
+             
+                  <tr key={user._id} className='bg-white dark:bg-table-row hover:tablerow-hover dark:hover:bg-tablerow-hover'>
+                    <td className='px-6 py-4 font-medium text-green-900'>{user._id}</td>
+                    <td className='px-6 py-4 text-green-900'>{user.name}</td>
+                    <td className='px-6 py-4 text-green-900'>{user.email}</td>
+                    <td className='px-6 py-4 text-green-900'>
+                      <img className='w-10 rounded-full' src={user.imageurl} alt='profile' />
+                    </td>
+                    <td className='px-6 py-4 text-green-900'>{user.role}</td>
+                    <td className='px-6 py-4 text-right text-green-900'>
+                      <Link to={`/e_updates/${user._id}`}>
+                        <button className='btn1 mr-3'>
+                          <FaEdit className='mr-5 text-xl' />
+                        </button>
+                      </Link>
+                      <button className='btn1' onClick={() => deleteuser(user._id)}>
+                        <MdDeleteForever className='mr-5 text-2xl' />
+                      </button>
+                    </td>
+                  </tr>
+                )))}
+                
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-    
-    </div>
-    <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-      
-      
-    </div>
-    </div>
-
-
-    </div>
-  )
+  );
 }
 
 export default Allusers;
-
-
-
-    
--- frontend/src/Screens/Employee/Euserprofile.js
+++ frontend/src/Screens/Employee/Euserprofile.js
@@ -7,7 +7,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 
 AOS.init({
-  duration:2500
+  duration:2000
 });
 
 
