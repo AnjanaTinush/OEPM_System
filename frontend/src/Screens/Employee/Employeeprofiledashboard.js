b186d5fa162a0db9d80caab27880e39afa4f5701
AnjanaTinush <anjanatinush222@gmail.com>
2024-04-04 15:50:17 +0530

    complete leave requseted

--- frontend/src/Screens/Employee/Employeeprofiledashboard.js
+++ frontend/src/Screens/Employee/Employeeprofiledashboard.js
@@ -1,91 +1,74 @@
 import React, { useState, useEffect } from 'react';
-import Navbar from '../Component/Navbar'
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import AdprofileNavbar from './Component/AdprofileNavbar';
-
 import clock from './image/clock.png';
 import leave from './image/leave.png';
 
 function Employeeprofiledashboard() {
-
   const [date, setDate] = useState(new Date());
-
   const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentTime(new Date());
-    }, 1000); // update every time
+    }, 1000); // update every second
 
     return () => clearInterval(intervalId); // Cleanup on component unmount
-  }, [])
+  }, []);
 
   return (
     <div>
-      <Navbar />
-      
-      <div className="max-w-2lg p-2 bg-white border border-gray-200 rounded-lg shadow m-1   relative">
-          
-          <div className="flex items-center">
-            <img src={leave} alt="Logo" className="w-11 mt-1 ml-4" />
-            <p className="ml-3 mt-1">
-              Total Leaves :{" "}
-              <span className="font-bold text-green-900">20</span>
-            </p>
-          </div>
-          <div className="absolute top-0 right-0 p-5">
-            <p className="text-whatsapp-green">
-              {currentTime.toLocaleTimeString()}
-            </p>
-          </div>
-
-          {/*  time display */}
-        </div>
-      <div className="justify-around flex">
       <AdprofileNavbar />
 
-      <div className="max-w-xl p-12 bg-white border-2 border-gray-300 rounded-lg shadow flex  mt-4 ml-6" style={{ width: '1230px' }}>
-      <div className="mt-4">
-    <Calendar
-      onChange={setDate}
-      value={date}
-      className="w-120 h-120 ml-12 " 
-    />
-  </div>
-</div>
-        
-    
-        {/*  Mark the Atten */}
-        <div class="max-w-sm  bg-white border border-gray-200 rounded-lg  shadow mx-auto">
-          <h1 class=" mb-4 front-boald">
-            <span class="font-bold">CHECK IN & CHECK OUT</span> Sri Lankan
-            Standard Time
+      <div className="p-2 ml-60 bg-white border border-gray-200 rounded-lg shadow m-1 relative">
+        <div className="flex items-center">
+          <img src={leave} alt="Logo" className="w-11 mt-1 ml-4" />
+          <p className="ml-3 mt-1">
+            Total Leaves : <span className="font-bold text-green-900">20</span>
+          </p>
+        </div>
+        <div className="absolute top-0 right-0 p-5">
+          <p className="text-whatsapp-green">{currentTime.toLocaleTimeString()}</p>
+        </div>
+      </div>
+
+      <div className="flex justify-between items-start">
+        <div className="ml-96  mt-32 bg-white border border-gray-200 rounded-lg shadow custom-height">
+          {/* Live Calendar Display */}
+          <Calendar
+            value={date}
+            onChange={setDate}
+            className="w-full h-full "
+          />
+        </div>
+
+        <div className="mt-20 mr-4 bg-white border border-gray-200 rounded-lg shadow h-96">
+          <h1 className="mb-4 font-bold">
+            CHECK IN & CHECK OUT Sri Lankan Standard Time
           </h1>
+          <hr className="border-gray-200" />
 
-          <hr className="" />
-
-          <div class="flex items-center ml-6">
-            <button class="w-24 mt-10 ml-3">
-              <p class="mt-2 text-whatsapp-green ">
+          <div className="flex items-center ml-6">
+            <button className="w-24 mt-16 ml-3">
+              <p className="mt-2 text-whatsapp-green">
                 {currentTime.toLocaleTimeString()}
               </p>
-              <p class="ml-3 ">Current In</p>
+              <p className="ml-3 mt-4">Current In</p>
               <img src={clock} alt="Logo" />
             </button>
-            <button class="w-24 mt-10 ml-32">
-              <p class="mt-2 text-whatsapp-green ">
+            <button className="w-24 mt-16 ml-32">
+              <p className="mt-2 text-whatsapp-green">
                 {currentTime.toLocaleTimeString()}
               </p>
-              <p class="ml-3 ">Current Out</p>
+              <p className="ml-3 mt-4">Current Out</p>
               <img src={clock} alt="Logo" />
             </button>
           </div>
         </div>
       </div>
     </div>
-
-  )
+  );
 }
 
-export default Employeeprofiledashboard
+export default Employeeprofiledashboard;
