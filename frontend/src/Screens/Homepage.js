c66856a116c92b6afdc24b34008478a2e582725b
AnjanaTinush <anjanatinush222@gmail.com>
2024-04-06 17:12:17 +0530

    .

--- frontend/src/Screens/Homepage.js
+++ frontend/src/Screens/Homepage.js
@@ -3,12 +3,11 @@ import { useLocation } from "react-router-dom";
 import Navbar from '../Component/Navbar.js'
 
 function Homepage() {
-
+//home page
   
   return (
     <div>
        <Navbar/>
-
     </div>
   )
 }
