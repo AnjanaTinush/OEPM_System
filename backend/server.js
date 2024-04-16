02510fc4924433e314712a9f7f97792536e83aff
DinilDinusahan <dinildinusahan@gmail.com>
2024-04-16 12:23:32 +0530

    yes

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ const driverRoute = require("./Routes/driverRoute")
 const leavesRoutes = require("./Routes/leavesRoutes")
 const customerOrderRoute = require("./Routes/customerOrderRoute")
 const shoppingCartRoute = require("./Routes/shoppingCartRoute")
+const targetRoutes = require("./Routes/targetsRoute")
+
 
 
 app.use(cors()); // Add this line to enable CORS
@@ -22,10 +24,12 @@ app.use("/api/tunnel", tunnelRoutes)
 app.use('/api/customeritems',customerOrderRoute);
 app.use("/api/shoppingCart",shoppingCartRoute);
 app.use("/api/drivers", driverRoute);
-app.use("/api/leaves",leavesRoutes);
+app.use("/api/target", targetRoutes)
+
+
 
 const port = process.env.PORT || 5000;
-
+app.use("/api/leaves",leavesRoutes);
 //app.use("/api/leaves" , leavesRoutes);
 
 
