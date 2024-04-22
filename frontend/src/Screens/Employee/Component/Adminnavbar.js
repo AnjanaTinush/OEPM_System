308d338a49f04fa7bd124c6501472e4bf8a4d48e
AnjanaTinush <anjanatinush222@gmail.com>
2024-04-23 00:38:03 +0530

    .

--- frontend/src/Screens/Employee/Component/Adminnavbar.js
+++ frontend/src/Screens/Employee/Component/Adminnavbar.js
@@ -14,7 +14,7 @@ function Adminnavbar() {
     
      
     <div className="bg-wight-green text-white h-screen w-1/6 fixed top-0 left-0 flex flex-col justify-between shadow-xl">
-    <div className="p-4 mt-2">
+    <div className="p-1 mt-2">
     <Navbar/>
         <Link            to="/employeeDashboard"
             className={`flex items-center w-full mt-6 py-3 px-4 ${location.pathname === "/employeeDashboard" ? 'bg-whatsapp-green text-white' : 'hover:bg-whatsapp-green text-green-900'} font-custom rounded-md text-decoration-none mb-2`}
@@ -24,14 +24,14 @@ function Adminnavbar() {
         </Link>
         <Link
             to="/e_allusers"
-            className={`flex items-center w-full mt-6 py-3 px-4 ${location.pathname === "/e_allusers" ? 'bg-whatsapp-green text-white' : 'hover:bg-whatsapp-green text-green-900'} font-custom rounded-md text-decoration-none mb-2`}
+            className={`flex items-center w-full mt-3 py-3 px-4 ${location.pathname === "/e_allusers" ? 'bg-whatsapp-green text-white' : 'hover:bg-whatsapp-green text-green-900'} font-custom rounded-md text-decoration-none mb-2`}
         >
             <FaUsers className="mr-5 text-lg" />
             <span className="font-bold text-lg">Users</span>
         </Link>
         <Link
             to="/e_approveleave"
-            className={`flex items-center w-full mt-6 py-3 px-4 ${location.pathname === "/e_approveleave" ? 'bg-whatsapp-green text-white' : 'hover:bg-whatsapp-green text-green-900'} font-custom rounded-md text-decoration-none mb-2`}
+            className={`flex items-center w-full mt-3 py-3 px-4 ${location.pathname === "/e_approveleave" ? 'bg-whatsapp-green text-white' : 'hover:bg-whatsapp-green text-green-900'} font-custom rounded-md text-decoration-none mb-2`}
         >
             <GiExitDoor className="mr-5 text-lg" />
             <span className="font-bold text-lg">leaves</span>
