dab40a70e55879c3848c92225caa71245c4eb70d
DinilDinusahan <dinildinusahan@gmail.com>
2024-05-08 23:25:03 +0530

    order details added

--- backend/Routes/scheduleorderRoute.js
+++ backend/Routes/scheduleorderRoute.js
@@ -2,27 +2,35 @@ const express = require('express');
 const router = express.Router();
 const ScheduleOrder = require('../models/scheduleorderModel');
 
-router.post("/newScheduleOrder",async (req,res)=>{
+// Route to mark an order as "Done"
+router.post("/markOrderAsDone/:orderId", async (req, res) => {
+    const orderId = req.params.orderId;
 
-    const {userid,itemName,price,quantity,date}=req.body;
-   
-    try{
-        const neworder=new ScheduleOrder({
-            userid,
-            itemName,
-            price,
-            quantity,
-            date            
+    try {
+        // Find the order by its ID
+        const order = await ScheduleOrder.findById(orderId);
+        if (!order) {
+            return res.status(404).json({ error: "Order not found" });
+        }
 
-        });
-        const result=await neworder.save() 
+        // Update the order status to "Done"
+        order.status = "Done";
+        await order.save();
 
-        res.send("Order requested successfully!");
-    }catch(error){
-
-        return res.status(400).json({error});
+        res.status(200).json({ message: "Order marked as done successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
     }
-})
+});
 
+// Route to fetch all orders
+router.get("/getallorders", async (req, res) => {
+    try {
+        const orders = await ScheduleOrder.find(); // Fetch all orders from the database
+        res.status(200).json(orders); // Return the orders as JSON response
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" }); // Return error if something goes wrong
+    }
+});
 
 module.exports = router;
--- frontend/src/Screens/Targets/OrderDetails.js
+++ frontend/src/Screens/Targets/OrderDetails.js
@@ -1,126 +1,130 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Adminnavbar from "./Component/Adminnavbar";
 import Loader from "../../Component/Loader";
+import { Tag } from "antd";
 
-function OrderDetails() {
+
+const OrderDetails = () => {
   const [orderDetails, setOrderDetails] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [order, setItems] = useState([]);
+  const [userCounter, setUserCounter] = useState({});
 
   useEffect(() => {
-    const fetchItems = async () => {
+    const fetchOrders = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:5000/api/getallorders"
-        );
-        setItems(response.data);
+        const response = await axios.get("/api/scheduleOrder/getallorders"); 
+        setOrderDetails(response.data);
       } catch (error) {
-        console.log(error);
+        console.error("Error fetching orders:", error);
       }
     };
 
-    fetchItems();
+    fetchOrders();
   }, []);
 
-  return (
-    <div>
-      {loading ? (
-        <Loader />
-      ) : (
-        <div className="bg-white">
-          <div className="flex flex-col w-full">
-            <h1 className="text-center text-3xl font-bold text-green-600 mt-8 mr-32">
-              Order Details
-            </h1>
-            <div className="flex justify-center items-center h-full mt-16 mb-16 mr-12">
-              <div className="overflow-x-auto shadow-2xl sm:rounded-lg">
-              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                      <thead className="text-xs text-whatsapp-green uppercase bg-wight-green dark:bg-whatsapp-green dark:text-wight-green">
-                        <tr>
-                          <th scope="col" className="px-6 py-3">
-                            Order ID
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            First Name
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Last Name
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Email
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Contact Number
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Street Address
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            City
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            District
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Postal Code
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Item Name
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Item ID
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            User ID
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Quantity
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Price
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Total Price
-                          </th>
-                          <th scope="col" className="px-6 py-3">
-                            Image URL
-                          </th>
-                        </tr>
-                      </thead>
+  const handleDone = async (orderId) => {
+    try {
+      // Call the backend API to mark the order as done
+      await axios.post(`/api/scheduleOrder/markOrderAsDone/${orderId}`);
+      // Update the status in the local state
+      const updatedOrderDetails = orderDetails.map(order => {
+        if (order._id === orderId) {
+          return { ...order, status: "Done" };
+        }
+        return order;
+      });
+      setOrderDetails(updatedOrderDetails);
+    } catch (error) {
+      console.error("Error marking order as done:", error);
+    }
+  };
 
-                      <tbody>
-                        {orderDetails.map((order) => (
-                          <tr
-                            key={order._id}
-                            className="bg-white dark:bg-table-row hover:tablerow-hover dark:hover:bg-tablerow-hover"
-                          >
-                            <td className="px-6 py-4">{order.orderID}</td>
-                            <td className="px-6 py-4">{order.firstName}</td>
-                            <td className="px-6 py-4">{order.lastName}</td>
-                            <td className="px-6 py-4">{order.email}</td>
-                            <td className="px-6 py-4">{order.contactNumber}</td>
-                            <td className="px-6 py-4">{order.streetAddress}</td>
-                            <td className="px-6 py-4">{order.city}</td>
-                            <td className="px-6 py-4">{order.district}</td>
-                            <td className="px-6 py-4">{order.postalCode}</td>
-                            <td className="px-6 py-4">{order.itemName}</td>
-                            <td className="px-6 py-4">{order.itemid}</td>
-                            <td className="px-6 py-4">{order.userid}</td>
-                            <td className="px-6 py-4">{order.quantity}</td>
-                            <td className="px-6 py-4">{order.price}</td>
-                            <td className="px-6 py-4">{order.totalprice}</td>
-                            <td className="px-6 py-4">{order.imageurl}</td>
-                          </tr>
-                        ))}
-                      </tbody>
-                    </table>
-              </div>
+  return (
+    <div className="bg-wight-green">
+      <div className="flex">
+        <Adminnavbar />
+        <div className="flex flex-col w-full">
+          <h1 className="text-center text-3xl font-bold text-green-600 mt-8 mr-32">
+            Order Details
+          </h1>
+          <div className="flex justify-center items-center h-full mt-16 mb-16 mr-12">
+            <div className="overflow-x-auto shadow-2xl sm:rounded-lg">
+              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-whatsapp-green uppercase bg-wight-green dark:bg-whatsapp-green dark:text-wight-green">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      Order ID
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      User ID
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Item Name
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Price
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Quantity
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Date
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Actions
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orderDetails.map((order, index) => (
+                    <tr
+                      key={order._id}
+                      className="bg-white dark:bg-table-row hover:tablerow-hover dark:hover:bg-tablerow-hover"
+                    >
+                      <td className="px-6 py-4 font-medium text-green-900 ">
+                        {index + 1}
+                      </td> {/* Display sequential numbering */}
+                      <td className="px-6 py-4 font-medium text-green-900 ">
+                        {/* Display user ID only if it's the first occurrence */}
+                        {userCounter[order.userid]
+                          ? userCounter[order.userid]
+                          : (userCounter[order.userid] = Object.keys(userCounter).length + 1)}
+                      </td>
+                      <td className="px-6 py-4 font-medium text-green-900 ">
+                        {order.itemName}
+                      </td>
+                      <td className="px-6 py-4 font-medium text-green-900 ">
+                        {order.price}
+                      </td>
+                      <td className="px-6 py-4 font-medium text-green-900 ">
+                        {order.quantity}
+                      </td>
+                      <td className="px-6 py-4 font-medium text-green-900 ">
+                        {new Date(order.date).toLocaleDateString()}
+                      </td> {/* Format Date */}
+                      <td className="px-6 py-4 font-medium text-green-900 ">
+                        <button
+                          onClick={() => handleDone(order._id)}
+                          className={`py-2 px-4 rounded ${
+                            order.status === "Done"
+                              ? "bg-gray-400 text-gray-800 cursor-not-allowed"
+                              : "bg-blue-500 hover:bg-blue-700 text-white font-bold"
+                          }`}
+                          disabled={order.status === "Done"}
+                        >
+                          {order.status === "Done" ? "Done" : "Mark as Done"}
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
-}
+};
 
 export default OrderDetails;
