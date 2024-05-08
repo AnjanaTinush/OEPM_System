69111e43fe574859c979a5d94858991b6e73b059
janithwijethunga <janithwijethunga68@gmail.com>
2024-05-08 18:20:48 +0530

    notification system

--- backend/Routes/driverfeedbackRoute.js
+++ backend/Routes/driverfeedbackRoute.js
@@ -1,7 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const driverfeedbackModel = require("../models/driverfeedbackModel");
+const nodemailer = require('nodemailer');
 
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587, 
+  secure: false, 
+  auth: {
+    user: 'janithbin@gmail.com', 
+    pass: 'cxlq xsvw xeix bpqd' 
+  }
+});
+
+
+
+// Send email notification function
+const sendEmailNotification = (feedback) => {
+  const mailOptions = {
+    from: 'janithbin@gmail.com',
+    to: 'janithwijethunga5@gmail.com',
+    subject: 'New feedback added',
+    text: `New feedback added for driver ${feedback.fdrname}.\nRating: ${feedback.frating}\nComment: ${feedback.fcomment}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+    } else {
+      console.log('Email sent:', info.response);
+    }
+  });
+};
+
+
+
+// New feedback
 // New feedback
 router.post("/newfeedback", async (req, res) => {
   const { fdrname, frating, fcomment } = req.body;
@@ -14,11 +48,14 @@ router.post("/newfeedback", async (req, res) => {
 
   try {
     await newFeedback.save();
+    sendEmailNotification(newFeedback); // Pass newFeedback object here
     res.send("Feedback submitted successfully");
+    
   } catch (error) {
-    return res.status(400).json({ error: error.message });
+    return res.status(400).json({ error });
   }
 });
+
 
 //get feedbacks
 router.get("/getallfeedbacks", async (req, res) => {
